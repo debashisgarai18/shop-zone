be988558ff59fe26df8c90d0821390ced5e20f11
DEBASHIS GARAI <debashis1805@gmail.com>
2024-10-25 22:30:58 +0530

    added one endpoint ot fetch the items given the category

--- backend/routes/commonRoutes.js
+++ backend/routes/commonRoutes.js
@@ -1,33 +1,53 @@
 // this comprises of the routes which are independent of the auth logic
-const {Router} = require("express");
+const { Router } = require("express");
 const { category } = require("../Database");
-const commonRouter = Router()
+const commonRouter = Router();
 
-// endpoint to get all the parentCategory names 
+// endpoint to get all the parentCategory names
 commonRouter.get("/getCategories", async (req, res) => {
-    try{
-        const getCategory = await category.find({})
-        let parentCategories = []; 
-        getCategory.map((e) => {
-            parentCategories.push({
-                cat : e.cat,
-                expand : e.expand,
-                sub : e.sub,
-                drops : e.drops,
-                logo: e.logo,
-                bgColor : e.bgColor
-            })
-        });
-        return res.status(200).json({
-            message : parentCategories
-        })
-    }catch(err){
-        return res.status(411).json({
-            message : `Bad Request, Error : ${err}`
-        })
-    }
-})
+  try {
+    const getCategory = await category.find({});
+    let parentCategories = [];
+    getCategory.map((e) => {
+      parentCategories.push({
+        cat: e.cat,
+        expand: e.expand,
+        sub: e.sub,
+        drops: e.drops,
+        logo: e.logo,
+        bgColor: e.bgColor,
+      });
+    });
+    return res.status(200).json({
+      message: parentCategories,
+    });
+  } catch (err) {
+    return res.status(411).json({
+      message: `Bad Request, Error : ${err}`,
+    });
+  }
+});
 
-// TODO : have one endpoint to fetch the items given a catgegory 
+// TODO : have one endpoint to fetch the items given a catgegory
+commonRouter.get("/getPRoducts/:category", async (req, res) => {
+  const getCat = req.params.category;
+  try {
+    const cat = await category.findOne({
+      cat: getCat,
+    });
+    if (!cat)
+      return res.status(404).json({
+        message: "No category found",
+      });
 
-module.exports = commonRouter 
+    return res.status(200).json({
+      message: cat.items,
+    });
+  } catch (err) {
+    return res.status(403).json({
+      message: `Some error occured : ${err}`,
+    });
+  }
+});
+
+module.exports = commonRouter;
